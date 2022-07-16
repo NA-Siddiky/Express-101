d9ada340c4ff482d6497acef4cf6830d671b4529
NA-Siddiky <siddiky.academic@gmail.com>
2022-07-17 01:11:41 +0600

    read html from file

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs');
+
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send(
-        `<h1>I am home route</h1>`
+    fs.readFile('./pages/index.html', (err, data) => {
+        if (err) {
+            console.log("Error", err);
+            res.send(`<h1>Something went wrong</h1>`);
+        }
+        else {
+            res.write(data);
+            res.end();
+        }
+    }
     )
 })
 app.get('/about', (req, res) => {
