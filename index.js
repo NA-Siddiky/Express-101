f65c51c01f44ea942ee24b279ee367bf0d0d5e92
NA-Siddiky <siddiky.academic@gmail.com>
2022-07-16 17:27:35 +0600

    adding html

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ app.get('/', (req, res) => {
         `<h1>I am home route</h1>`
     )
 })
-
+app.get('/about', (req, res) => {
+    res.send(
+        `<h1>I am about route</h1>`
+    )
+})
+app.get('/help', (req, res) => {
+    res.send(
+        `<h1>I am help route</h1>`
+    )
+})
 
 const PORT = 8080;
 app.listen(PORT, () => {
