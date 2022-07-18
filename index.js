3186f3c80102142bff86ea3189297ca050325f4c
NA-Siddiky <siddiky.academic@gmail.com>
2022-07-18 16:01:25 +0600

    adding middleware.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const fs = require('fs');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express()
+
+//middleware(global - work on every router)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(cors());
 
 app.get('/', (req, res) => {
     console.log(req.url)
