afe4fc25f923bb981f4bfddab20890a735ae6833
NA-Siddiky <siddiky.academic@gmail.com>
2022-07-18 16:10:36 +0600

    createing middleware.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(cors());
+app.use(globalMiddleware);
 
 app.get('/', (req, res) => {
     console.log(req.url)
@@ -61,3 +62,11 @@ const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`);
 });
+
+
+//creating customs middleware
+function globalMiddleware(req, res, next) {
+    console.log(`${req.method} - ${req.url}`);
+    console.log("global middleware");
+    next();
+}
